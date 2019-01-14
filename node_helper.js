60fcfb60a5be585a9cd0570d4a8143483495e37a
Rich Alberth <ralberth@amazon.com>
2019-01-13 21:01:38 -0500

    Destroy SQL connections so they don't pile up

--- node_helper.js
+++ node_helper.js
@@ -23,16 +23,39 @@ module.exports = NodeHelper.create({
             this.debuglog("Received " + JSON.stringify(payload, null, 2));
             var con = mysql.createConnection(payload.connection);
             con.connect(function(err) {
-                if (err) throw err;
+                if (err) {
+                    con.destroy();
+                    throw err;
+                }
                 con.query(payload.query, function(err, result) {
-                    if (err) throw err;
+                    if (err) {
+                        con.destroy();
+                        throw err;
+                    }
                     helper.sendSocketNotification("MYSQLQUERY_RESULT", {
                         identifier: payload.identifier,
                         rows: result
                     });
+
                     helper.debuglog("Sending result: " + JSON.stringify(result, null, 2));
                 });
             });
+
+            /*
+             * Annoying part of Socket.io used by MM core logic: it doesn't support promise chaining, so you
+             * can't put a promise on the completion of sendSocketNofication() above.  You can't just call
+             * con.destroy() here, or the connection to the database will be closed *before* the data has been
+             * consumed and sent to the browser!  So, we have a total HACK here: wait a few seconds for the
+             * data to presumably be sent, then close the connection.  YES, this is a race condition.  LOSE.
+             */
+            setTimeout(
+                function(con) {
+                    helper.debuglog("Destroying unneeded SQL connection");
+                    con.destroy();
+                },
+                10000,
+                con
+            );
         }
     }
 });
